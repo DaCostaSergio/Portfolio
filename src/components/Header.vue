11a9b10394566d0b6dbd91c0679b89c9d1115cdc
DaCostaSergio <47104470+DaCostaSergio@users.noreply.github.com>
2022-07-28 11:55:08 +0200

    NavBar responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,89 @@
 <template>
-  <div class="text-white mr-6 flex flex-row">
-    <div class="text-4xl m-10 font-N27">
-      SERGIO DA COSTA
-    </div>
+  <div>
+    <div class="bg-black">
+      <nav
+          class="
+          container
+          px-6
+          py-8
+          mx-auto
+          md:flex md:justify-between md:items-center
+        "
+      >
+        <div class="flex items-center justify-between">
+          <router-link
+              to="/"
+              class="
+              text-xl
+              font-N27
+              font-bold
+              text-white
+              md:text-4xl
+              hover:text-blue-400
+            "
+          >
+            Sergio Da Costa
+          </router-link>
 
-
-    <section class="flex  flex-row justify-end m-10 text-3xl font-N27">
-      <div class="  text-3xl font-N27">
-        contact@sergiodacosta.ch
-      </div>
-      <section class="ml-32 flex flex-row">
-        <div class="flex flex-end">
-          <div class="mr-5">
-            Home
-          </div>
-          <div class="mr-5">
-            Projects
-          </div>
-          <div class="mr-5">
-            About
+          <!-- Mobile menu button -->
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
+            <button
+                type="button"
+                class="
+                text-white
+                hover:text-gray-400
+                focus:outline-none focus:text-white
+              "
+            >
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path
+                    fill-rule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
           </div>
         </div>
-      </section>
-    </section>
+
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul
+            :class="showMenu ? 'flex' : 'hidden'"
+            class="
+            flex-col
+            mt-8
+            space-y-4
+            md:flex
+            md:space-y-0
+            md:flex-row
+            md:items-center
+            md:space-x-10
+            md:mt-0
+          "
+        >
+          <li class="text-2xl text-white font-N27  hover:text-blue-400">
+            Home
+          </li>
+          <li class="text-2xl text-white font-N27  hover:text-blue-400">
+            Projects
+          </li>
+          <li class="text-2xl text-white font-N27  hover:text-blue-400">
+            About
+          </li>
+          <li class="text-2xl text-white font-N27  hover:text-blue-400">
+            Email
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "Header.vue"
-}
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
 </script>
-
