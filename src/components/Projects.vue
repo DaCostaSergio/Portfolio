7bc7b0234caac6d9ea10ac557888b6a3ef657f6a
DaCostaSergio <47104470+DaCostaSergio@users.noreply.github.com>
2022-08-02 19:02:19 +0200

    first project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,41 +1,15 @@
 <script>
-
-import Sia from '../components/Projects/Sia.vue';
-import Asap from '../components/Projects/Asap.vue';
-
-const routes = {
-  '/':Asap,
-  '/sia':Sia,
-
-
-}
 </script>
 
 <template>
-  <div>
-    <div class="bg-white divide-x-[3px]">
-      <nav
-          class="
-          container
-          px-6
-          py-8
-          mx-auto
-          md:flex md:justify-between md:items-center
-        "
-      >
-        <ul>
-          <li class="text-2xl text-black font-N27 w-20  hover:underline" >
-            <router-link to="#/sia">
-              <a href="#/sia">
-                <img src="../Medias/GR371_DaCosta_Sergio_TPP_Affiche.jpg" alt="">
-              </a>
-            </router-link>
-          </li>
-        </ul>
-      </nav>
+  <div class="m-20 mb-5">
+    <div class="font-N27 text-2xl">
+      Asap Rocky Project <br>
+      Date : 2020
+    <a href="https://asaprockyproject.netlify.app">
+      <img src="../assets/Asap_Project.png" alt="AsapProject" class="w-96">
+    </a>
     </div>
+
   </div>
-  <div class="border-y-black">
-  </div>
-  <component :is="currentView" />
 </template>
